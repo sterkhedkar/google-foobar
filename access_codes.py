7ccaca8b0e8392012892b3eae076e714e299c5dc
Sumit Terkhedkar <sumitterkhedkar@gmail.com>
2017-11-20 13:33:01 +0530

    Create access_codes.py

---
+++ access_codes.py
@@ -0,0 +1,23 @@
+
+"""
+  *** The basic Skeleton ***
+"""
+
+class AccessCodes:
+  
+  def __init(self)__:
+    print "___Initialized___"
+    
+  def create_smaller_groups:
+    pass
+    
+  def get_current_access_token:
+    pass
+    
+  def run:
+    pass
+    
+    
+    
+access_codes_instance = AccessCodes()
+print access_codes_instance.run()
